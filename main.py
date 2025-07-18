ccfa3f486610e5278299a8c8e0735e6d501f28b9
idiakite65 <154091747+idiakite65@users.noreply.github.com>
2025-07-18 12:40:36 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-bbbb
-from flet import *
+--- ============ table DG ==================
+create table dg(
+    id_dg serial,
+    nom_dg varchar(35),
+    prenom_dg varchar(35),
+    email_dg varchar(65),
+    tel_dg varchar(85),
+    pass_dg varchar(250),
+    pass_dg_info varchar(50)
+);
+--- ============ end table DG ==================
 
-def main(page: Page):
-  T Text('Rakwan ali')
-  page.add(T)
-  
-  page.update()
 
-app (main)
+--- ============ contraint table DG ==================
+alter table dg add constraint dg_pkey primary key (id_dg);
+alter table dg  add constraint not null email_dg;
+alter table dg  add constraint not null pass_dg;
+alter table dg  add constraint dg_unique unique (nom_dg,)
+--- ============ ennd contraint table DG ==================
