1f7a9f76aba7197d5e09eaa9fb904996d38cece7
idiakite65 <154091747+idiakite65@users.noreply.github.com>
2025-07-18 14:08:02 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ create table CLASSES(
     id_cl serial,
     nom_cl varchar(35),
     Coef  int,
-    Niveau varchar(10),
-    
+    Niveau varchar(10),    
 );
 --- ============ end table CLASSES ==================
 
@@ -15,6 +14,8 @@ alter table CLASSES  add constraint not null nom_cl;
 alter table dg  CLASSES constraint not null Coef;
 alter table dg  CLASSES constraint dg_unique unique (nom_cl)
 --- ============ ennd contraint table CLASSES ==================
+
+
 
 --- ============ table SERIES ==================
 create table SERIES(
@@ -28,6 +29,8 @@ alter table SERIES  add constraint not null nom_se;
 alter table dg  SERIES constraint se_unique unique (nom_se)
 alter table SERIES constraint se_fkey foreign key (id_se) references to (ECOLE) on update cascade on delete cascade;
 --- ============ ennd contraint table SERIES ==================
+
+
 
 --- ============ table MATIERES ==================
 create table MATIERES(
@@ -44,6 +47,8 @@ alter table MATIERES  add constraint not null nom_ma2;
 alter table MATIERES constraint ma_unique unique (nom_ma1)
 alter table MATIERES constraint ma_fkey foreign key (id_ma) references to (ECOLE) on update cascade on delete cascade;
 --- ============ ennd contraint table MATIERES ==================
+
+
 
 
 --- ============ table MATIERECLASSE ==================
