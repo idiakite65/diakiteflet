84530c79b462ec18eacab89ae8f6a31d6f113c4c
idiakite65 <154091747+idiakite65@users.noreply.github.com>
2025-07-18 13:46:46 +0000

    Update main.py

--- main.py
+++ main.py
@@ -20,12 +20,26 @@ alter table dg  CLASSES constraint dg_unique unique (nom_cl)
 create table SERIES(
     id_se serial,
     nom_se varchar(35),
-    id_ecol  int,   
-    
+    id_ecol  int, 
 );
 --- ============ contraint table SERIES ==================
 alter table SERIES add constraint se_pkey primary key (id_se);
 alter table SERIES  add constraint not null nom_se;
 alter table dg  SERIES constraint se_unique unique (nom_se)
 alter table SERIES constraint se_fkey foreign key (id_se) references to (ECOLE) on update cascade on delete cascade;
---- ============ ennd contraint table CLASSES ==================
+--- ============ ennd contraint table SERIES ==================
+
+--- ============ table MATIERES ==================
+create table MATIERES(
+    id_ma serial,
+    nom_ma1 varchar(505),
+    nom_ma2 varchar(505),
+    coef_ma int,
+    id_ecol  int, 
+);
+--- ============ contraint table SERIES ==================
+alter table MATIERES add constraint se_pkey primary key (id_se);
+alter table MATIERES  add constraint not null nom_se;
+alter table dg  MATIERES constraint se_unique unique (nom_se)
+alter table MATIERES constraint se_fkey foreign key (id_se) references to (ECOLE) on update cascade on delete cascade;
+--- ============ ennd contraint table SERIES ==================
